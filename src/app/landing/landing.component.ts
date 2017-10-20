945b6ae37e5ccc6197f545f8a545deb43cf7a04b
Kevin Dufendach <KevinDufendach@users.noreply.github.com>
2017-10-20 08:56:15 -0400

    clean files

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -11,30 +11,25 @@ export class LandingComponent implements OnInit, OnDestroy {
   state: string;
   code: string;
   private sub: any;
-  id: number;
-  params: any;
-  dynId: string;
+  urlParams: any;
 
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.sub = this.route.queryParams.subscribe(params => {
-      // this.id = +params['id'];
-      // console.log(params['id']);
-      this.params = params;
+      this.urlParams = params;
     });
 
-    this.dynId = this.getUrlParameter('id');
     this.state = this.getUrlParameter('state');
     this.code = this.getUrlParameter('code');
   }
 
   private getUrlParameter(sParam: string): string {
-    if (isUndefined(this.params[sParam])) {
+    if (isUndefined(this.urlParams[sParam])) {
       console.log('parameter ' + sParam + ' does not exist');
       return '';
     }
-    return this.params[sParam];
+    return this.urlParams[sParam];
   }
 
   ngOnDestroy() {
